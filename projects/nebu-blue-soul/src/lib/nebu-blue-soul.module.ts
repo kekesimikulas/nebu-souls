d409dfe26df000b76610011f6d52f8f1fd9008fc
godzzo <egodzzo@gmail.com>
2020-10-30 21:19:17 +0100

    Add nebular starter fow showcase project

--- projects/nebu-blue-soul/src/lib/nebu-blue-soul.module.ts
+++ projects/nebu-blue-soul/src/lib/nebu-blue-soul.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { NebuBlueSoulComponent } from './nebu-blue-soul.component';
-
+import { NbThemeModule } from '@nebular/theme';
 
 
 @NgModule({
   declarations: [NebuBlueSoulComponent],
   imports: [
+    NbThemeModule.forRoot(),
   ],
   exports: [NebuBlueSoulComponent]
 })
---
+++ projects/nebu-souls-showcase/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NbLayoutModule, NbThemeModule } from '@nebular/theme';
+import { NbSidebarModule } from '@nebular/theme';
+import { NbButtonModule } from '@nebular/theme';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+
+    NbThemeModule.forRoot(),
+    NbLayoutModule,
+    NbSidebarModule.forRoot(), // NbSidebarModule.forRoot(), //if this is your app.module
+    NbButtonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
